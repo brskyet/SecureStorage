e4c839507cf845eccb5afc8f872a1e4dd9bed031
xwalab <48030593+xwalab@users.noreply.github.com>
2020-02-05 17:36:07 +0300

    Add DB

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SecureStorage.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDBContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Storage.db");
+        }
+    }
+}
---
+++ Data/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureStorage.Data
+{
+    public class User
+    {
+        public Guid UserId { set; get; }
+        public string Login { set; get; }
+        public string Password { set; get; }
+        public int Age { set; get; }
+    }
+}
